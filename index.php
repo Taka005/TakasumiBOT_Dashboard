769a00d25bd04a4c07e0f1ea3c501cfdae996b30
Taka005 <01taka005@gmail.com>
2022-12-13 11:50:51 +0900

    ファビコン設定

--- index.php
+++ index.php
@@ -10,6 +10,13 @@ require_once __DIR__."/config.php";
 
         <title>TakasumiBOT</title>
 
+        <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="./assets/img/apple-touch-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon-16x16.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./assets/img/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="180x180" href="./assets/img/apple-touch-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="./assets/img/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="1024x1024" href="./assets/img/takasumibot.png">
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="./assets/css/style.css">
     </head>
