d81e8f4f208932e456b475b771e230e6061f4a10
Taka005 <01taka005@gmail.com>
2022-12-14 19:27:19 +0900

    修正

--- system/login.php
+++ system/login.php
@@ -3,7 +3,8 @@ require_once __DIR__."/discord.php";
 require_once __DIR__."/../config.php";
 
 init($redirect_url,$client_id,$secret_id);
-get_info();
+get_user();
+get_guilds();
 
 header("Location: ../");
 exit;
