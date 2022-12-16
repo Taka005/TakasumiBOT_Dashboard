bbd44bc9c4954257a611dce669b7198b25775a5a
Taka005 <01taka005@gmail.com>
2022-12-16 09:07:49 +0900

    パス修正

--- server/id.php
+++ server/id.php
@@ -12,21 +12,21 @@ require_once __DIR__."/../system/lib.php";
         <title>TakasumiBOT</title>
 
         <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="./assets/img/apple-touch-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon-16x16.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="./assets/img/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="180x180" href="./assets/img/apple-touch-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192" href="./assets/img/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="1024x1024" href="./assets/img/takasumibot.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicon-16x16.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="180x180" href="../assets/img/apple-touch-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="../assets/img/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="1024x1024" href="../assets/img/takasumibot.png">
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="./assets/css/style.css">
+        <link rel="stylesheet" href="../assets/css/style.css">
     </head>
     <body>
         <header>
             <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
                 <div class="container-fluid">
                     <a class="navbar-brand text-darl" href="./">
-                        <img src="./assets/img/takasumibot.png" alt="ロゴ" width="30" height="30" class="d-inline-block align-text-top">
+                        <img src="../assets/img/takasumibot.png" alt="ロゴ" width="30" height="30" class="d-inline-block align-text-top">
                         TakasumiBOT
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false">
@@ -57,9 +57,9 @@ require_once __DIR__."/../system/lib.php";
                                     <?= $_SESSION["username"]."#".$_SESSION["discrim"] ?>
                                 </button>
                                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                    <li><a class="dropdown-item" href="./account">アカウント</a></li>
+                                    <li><a class="dropdown-item" href="../account">アカウント</a></li>
                                     <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item text-danger" href="./system/logout">ログアウト</a></li>
+                                    <li><a class="dropdown-item text-danger" href="../system/logout">ログアウト</a></li>
                                     <li><a class="dropdown-item text-primary" href="<?= url($client_id,$redirect_url,$scopes) ?>">データ同期</a></li>
                                 </ul>
                             </div>
@@ -79,7 +79,7 @@ require_once __DIR__."/../system/lib.php";
                 <?php } ?>
             </div>
 	    </main>
-        <script src="./assets/js/script.js"></script>
+        <script src="../assets/js/script.js"></script>
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
