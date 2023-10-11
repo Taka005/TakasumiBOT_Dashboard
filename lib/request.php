4bef44426a2670bb9b40cad221eaee2e3364b7f6
Taka005 <01taka005@gmail.com>
2023-10-11 20:25:08 +0900

    fix

--- lib/request.php
+++ lib/request.php
@@ -9,12 +9,11 @@ function GET($url,$header){
     return $res;
 }
 
-function POST($url,$data,$header){
+function POST($url,$data){
     $curl = curl_init();
     curl_setopt($curl,CURLOPT_URL,$url);
     curl_setopt($curl,CURLOPT_POST,true);
     curl_setopt($curl,CURLOPT_POSTFIELDS,http_build_query($data));
-    if($header) curl_setopt($curl,CURLOPT_HTTPHEADER,$header);
     curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
     $res = json_decode(curl_exec($curl),true);
     curl_close($curl);
